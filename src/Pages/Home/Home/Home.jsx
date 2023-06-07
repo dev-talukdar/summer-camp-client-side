1065895f056894a8132e6a616e21ac280afb151a
dev.talukdar <khejurbd@gmail.com>
2023-06-07 22:07:39 +0600

    carousel added on the homepage

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from "../Banner/Banner";
+
  
 
 const Home = () => {
     return (
         <div>
-           <h2>This is Home </h2> 
+          <Banner></Banner>
         </div>
     );
 };
